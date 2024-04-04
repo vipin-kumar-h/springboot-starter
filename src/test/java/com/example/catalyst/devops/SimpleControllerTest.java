b8772a2ef979d825ded1e08da44cf948d278acb1
thangaram.s <thangaram.s@zohocorp.com>
2024-04-04 17:29:03 +0530

    controller test

--- src/test/java/com/example/catalyst/devops/SimpleControllerTest.java
+++ src/test/java/com/example/catalyst/devops/SimpleControllerTest.java
@@ -19,6 +19,6 @@ class SimpleControllerTest {
   @Test
   public void shouldReturnHelloWorld() throws Exception {
     this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-      .andExpect(content().string("Hello World i'm build in Azure Pipelines!"));
+      .andExpect(content().string("Hello World i'm build in Catalyst Pipelines!"));
   }
 }
